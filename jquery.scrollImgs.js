f43c485ff0a6e8d05cb16a4fe143a321a3191536
@Life <udbmnm@163.com>
2014-08-18 14:14:56 +0800

    修改手动

--- jquery.scrollImgs.js
+++ jquery.scrollImgs.js
@@ -1,9 +1,9 @@
 /**
  * @Description jQuery无缝滚动内容插件
- * @author udbmnm@163.com
+ * @author admin@f2e.be
  * @DOM
  *  	<div id="scroll">
- *  		<ul>
+ *  		<ul id="scrollCon">
  *   			<li></li>
  *   			<li></li>
  *  		</ul>
@@ -16,6 +16,7 @@
  *		</span>		
  * @CSS
  *  	#scroll {overflow:hidden;width:200px;height:50px;}
+ * 		#scrollCon{position:relative;left:0;top:0;}
  * @Usage
  *  	$('#scroll').scrollImgs({
  *	    	prevBtn : $('#prevBtn'),
@@ -25,24 +26,28 @@
  *
  * @options
  *		speed:'slow',	//滚动速度fast slow 毫秒数
- *  	delayTime:2000,	//滚动间隔时间 默认2000ms
+ *  	delayTime:2000,	//滚动间隔时间 默认3000ms
  *		event:'click',	//手动触发事件
  *		auto:true		//是否自动开始滚动 默认true
- *		direction	//滚动方向left左右滚动,top上下滚动 默认left
+ *		direction:left	//滚动方向left左右滚动,top上下滚动 默认left
+ * 		manual:true 	//是否需要手动滚动，默认true
  */
+
 ;(function($){
+	
 	$.fn.scrollImgs = function(options) { 
 
 		var defualts = {
 			speed: 'slow',
-			delayTime: 2000,
+			delayTime: 3000,
 			event: 'click',
 			auto : true,
-			direction : 'left'
+			direction : 'left',
+			manual : true
 		};
 
 		var opts = $.extend({}, defualts, options),
-			$ul = this.children(),
+			$ul = this.children('#scrollCon'),
 			$li = $ul.children(),
 			$prev = opts.prevBtn,
 			$next = opts.nextBtn,
@@ -59,12 +64,12 @@
 		if(mode) {
 			distance = $li.outerWidth();
 			$ul.width(distance*(listNum+2));
-			var nextMode = { left : '-='+distance+'px'},
+			var nextMode = { left : '-'+distance+'px'},
 			prevMode =  { left: 0};
 		}else{
 			distance = $li.outerHeight();
 			$ul.height(distance*(listNum+2));
-			var nextMode = { top : '-='+distance+'px'},
+			var nextMode = { top : '-'+distance+'px'},
 			prevMode =  { top: 0};
 		}
 
@@ -76,28 +81,24 @@
 						fun.autoScroll();
 					}, delayTime);
 				}
+				if(!opts.manual) return;
 				this.manual();
 			},
 			autoScroll : function() {
 				fun.clear();
-				$ul.stop(true, true);
-				var eles = $ul.children(':first-child');
-				eles.clone().appendTo($ul);
 				$ul.animate(nextMode,speed,function() {
+					$ul.children(':first').remove().clone().appendTo($ul);
 					$ul.css(direction, 0);
-					eles.remove();
 				});
-				
 				t1 = setTimeout(arguments.callee, delayTime);
 			},
 
 			manual : function() {
 				$prev.off().on(opts.event,function(){
 					fun.clear();
-					$ul.stop(true, true); 
-					var eles = $ul.children(':last-child');
+					var eles = $ul.children(':last');
 					eles.clone().prependTo($ul);
-					$ul.css(direction,-distance).animate(prevMode,speed,function() {
+					$ul.stop(true, true).css(direction,-distance).animate(prevMode,speed,function() {
 							eles.remove();
 						});
 					t2 = setTimeout(arguments.callee, delayTime);
